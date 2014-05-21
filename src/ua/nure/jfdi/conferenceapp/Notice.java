687f2647d8e686b82992e55270f9c36dd6760b7b
Nikita Doyun <doyun.nikita@gmail.com>
2014-05-21 18:50:34 +0300

    Change gitignore, delete test, modified Notice and FeedFragment.

--- src/ua/nure/jfdi/conferenceapp/Notice.java
+++ src/ua/nure/jfdi/conferenceapp/Notice.java
@@ -1,16 +1,24 @@
 package ua.nure.jfdi.conferenceapp;
 
+import java.util.Date;
+
 import android.media.Image;
 
 public class Notice {
 	private String title;
 	private String text;
 	private Image image;
+	private Date date;
 
-	public Notice(String title, String text, Image image) {
+	public Notice(String title, String text, Image image, long date) {
 		this.image = image;
 		this.title = title;
 		this.text = text;
+		this.date = new Date(date);
+	}
+
+	public Date getDate() {
+		return date;
 	}
 
 	public String getTitle() {
