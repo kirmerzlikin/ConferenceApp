2398866e079805d6f27639dd4287b8adab6c35c6
KirMerzlikin <kirmerzlikin@yandex.ru>
2014-05-22 23:46:06 +0300

    chathandler + connectioncreator

--- src/ua/nure/jfdi/conferenceapp/model/ConnectionCreator.java
+++ src/ua/nure/jfdi/conferenceapp/model/ConnectionCreator.java
@@ -1,20 +1,45 @@
 package ua.nure.jfdi.conferenceapp.model;
 
+import java.io.IOException;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class ConnectionCreator {
 
-	Socket socket;
+	
 
 	private boolean getInfoByMac(String macAddress) {
+		//запрос здесь
 		return true;
 	}
 
 	public boolean initializeConnection(String macAddress, FeedHandler fH,
-			ChatHandler cH) {
+			final ChatHandler cH) {
 		if (!getInfoByMac(macAddress))
 			return false;
-
+		
+		Thread connectionThread = new Thread(new Runnable(){
+			@Override
+			public void run(){
+				try {
+					Socket socket = new Socket(/*"159.253.134.253", 20340*/"10.0.2.2", 50000);
+					cH.setSocketConnection(socket);
+				} catch (UnknownHostException e) {
+					// TODO Auto-generated catch block
+					} catch (IOException e) {
+					// TODO Auto-generated catch block
+					}
+			}
+		});
+		
+		connectionThread.start();
+		
+		try {
+			connectionThread.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+		}
+		
 		return true;
 
 	}
